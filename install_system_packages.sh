b2b8fe27df57ba1657b02108ed2a195a380c3a8b
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-08-14 15:21:56 -0600

    Added fish to LazyVim dependencies

--- install_system_packages.sh
+++ install_system_packages.sh
@@ -46,6 +46,7 @@ ubuntu)
     python3-neovim \
     python3-pip \
     luarocks \
+    fish \
     -y
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
