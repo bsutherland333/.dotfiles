c01b2bbb73041a014c854aff5deb92b8f2370630
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-08-13 18:20:23 -0600

    added autosave and system keyboard access

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Write files when focus is lost
+vim.api.nvim_create_autocmd("FocusLost", {
+  callback = function()
+    vim.cmd("wa")
+  end,
+})
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set distance from edges
 vim.opt.scrolloff = 5
+
+-- Let vim use system keyboard
+vim.opt.clipboard = "unnamedplus"
