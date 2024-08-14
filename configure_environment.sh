1d8a1375f40b2fdf4d31cb1eddcf899be634aa70
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-08-14 14:44:34 -0600

    Moved oh-my-zsh install before .zshrc link

--- configure_environment.sh
+++ configure_environment.sh
@@ -5,6 +5,7 @@ echo "Installing dotfiles, installing user packages, and configuring system."
 export DOTFILES="$HOME/.dotfiles"
 
 ## zsh ##
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -sf "$DOTFILES/zsh/.zshrc" "$HOME"
 
 ## vim ##
@@ -18,9 +19,6 @@ ln -sf "$DOTFILES/nvim" "$HOME/.config"
 ## alacritty ##
 mkdir -p "$HOME/.config/alacritty"
 ln -sf "$DOTFILES/alacritty/alacritty.toml" "$HOME/.config/alacritty"
-
-## oh my zsh ##
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ## diff-so-fancy ##
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RF"
