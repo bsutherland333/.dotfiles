b078e94a8b9fc83bd9cf92521b681d9284455fbd
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-08-17 15:45:26 -0600

    make diff-so-fancy config still work when package is not avaliable

--- configure_environment.sh
+++ configure_environment.sh
@@ -21,7 +21,25 @@ mkdir -p "$HOME/.config/alacritty"
 ln -sf "$DOTFILES/alacritty/alacritty.toml" "$HOME/.config/alacritty"
 
 ## diff-so-fancy ##
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RF"
-git config --global interactive.diffFilter "diff-so-fancy --patch"
+git config --global core.pager "command -v diff-so-fancy >/dev/null 2>&1 && diff-so-fancy | less --tabs=4 -RF || less"
+git config interactive.diffFilter "$HOME/.config/diff-so-fancy/dsf-filter"
+
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.func       "146 bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+mkdir -p "$HOME/.config/diff-so-fancy"
+ln -sf "$DOTFILES/diff-so-fancy/dsf-filter" "$HOME/.config/diff-so-fancy"
 
 echo "Configuration complete."
