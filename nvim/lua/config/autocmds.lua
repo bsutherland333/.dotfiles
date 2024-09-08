b704b93d760b87ec1a28a630b86b812390a403ef
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-09-07 18:25:55 -0600

    Added improved autosave autocmd

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,13 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Write files when focus is lost
-vim.api.nvim_create_autocmd("FocusLost", {
-  callback = function()
-    vim.cmd("wa")
-  end,
-})
-
 -- Use a plain text syntax for Markdown files
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
@@ -25,4 +18,25 @@ vim.api.nvim_create_autocmd("FileType", {
       enabled = false,
     })
   end,
+})
+
+-- Autosave during editing
+local function save()
+  local buf = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_buf_call(buf, function()
+    vim.cmd("silent! write")
+  end)
+end
+
+vim.api.nvim_create_augroup("AutoSave", {
+  clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  callback = function()
+    save()
+  end,
+  pattern = "*",
+  group = "AutoSave",
 })
